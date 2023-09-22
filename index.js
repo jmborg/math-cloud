418bc5c705d538bda7a3b231b33b02c9d9f41156
Johan Borg <johanborg@MacBook-Pro-som-tillhor-Johan.local>
2023-09-22 20:16:45 +0200

    update

---
+++ index.js
@@ -0,0 +1,37 @@
+const firstOperand = document.getElementById('first-operand')
+const secondOperand = document.getElementById('second-operand')
+const startGame = document.querySelector('.start-game')
+const btns = document.querySelectorAll('.operator')
+
+let selectedOperator = ""
+let correctAnswers = 0
+
+btns.forEach(function (i) {
+    i.addEventListener('click', function() {
+        selectedOperator = i.textContent
+        document.querySelector('.operator-selected').innerHTML = i.innerHTML
+    })
+})
+
+function calculateRandomNumber () {
+    return e = Math.floor(Math.random() * 11)
+}
+
+startGame.addEventListener('click', function() {
+    firstOperand.textContent = calculateRandomNumber()
+    secondOperand.textContent = calculateRandomNumber()
+    switch (selectedOperator) {
+        case "+":
+            console.log("You've chosen the addition operator")
+            break
+        case "-":
+            console.log("You've chosen the subtraction operator")
+            break
+        case "x":
+            console.log("You've chosen the multiply operator")
+            break
+        case "/":
+            console.log("You've chosen the division operator")
+            break
+    }
+})
