3471676bbde57626e297ced779e53741fc6a5ba7
Johan Borg <johanborg@MacBook-Pro-som-tillhor-Johan.local>
2023-10-01 23:35:26 +0200

    last

--- index.js
+++ index.js
@@ -11,25 +11,37 @@ let answerColors = {
 
 let result = ""
 
-startBtn.addEventListener('click', function() {
+function start() {
+    resultInput.focus()
     let num1 = calculateRandomNumber()
     let num2 = calculateRandomNumber()
     firstOperand.textContent = num1
     secondOperand.textContent = num2
     result = num1 + num2
-})
+}
+
+function reset() {
+    result = ""
+    resultInput.value = ""
+}
 
 function calculateRandomNumber () {
     return e = Math.floor(Math.random() * 11)
 }
 
+startBtn.addEventListener('click', function() {
+    start()
+})
+
 resultInput.addEventListener("input", function() {
     let answer =+ resultInput.value
     if (answer === result) {
-        resultInput.style.border = answerColors.correct
+        resultInput.style.border = answerColors.blank
+        reset()
+        start()
      } else if (answer != result) {
         resultInput.style.border = answerColors.incorrect
-    } else if (answer = 0) {
+    } else if (answer = 0) {    // Denna gör inget ännu
         resultInput.style.border = answerColors.blank
     }
 })
